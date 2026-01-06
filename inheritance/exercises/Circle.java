9980b5d2fd90f917e1bd4e4b2d16ad6b647bd983
sssh199x <sandeshhamal5890@gmail.com>
2026-01-06 13:15:25 +0545

    add: string_options: String vs String Builder and Cylinder class with Circle Inheritance.

---
+++ inheritance/exercises/Circle.java
@@ -0,0 +1,23 @@
+package inheritance.exercises;
+
+public class Circle {
+    // write code here
+    private double radius;
+
+    public Circle(double radius){
+        if(radius < 0){
+            this.radius = 0;
+            return;
+        }
+        this.radius = radius;
+    }
+
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getArea(){
+        return (Math.PI * radius * radius);
+    }
+}
---
+++ inheritance/exercises/Cylinder.java
@@ -0,0 +1,23 @@
+package inheritance.exercises;
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(double radius , double height){
+        super(radius);
+        if(height < 0){
+            this.height = 0;
+            return;
+        }
+
+        this.height = height;
+    }
+
+
+    public double getHeight(){
+        return height;
+    }
+
+    public double getVolume(){
+        return (super.getArea() * height);
+    }
+}
