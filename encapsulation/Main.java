8a29cdaa77e45592e2a0a05f2bd5f2cab6799526
sssh199x <sandeshhamal5890@gmail.com>
2026-01-13 20:12:03 +0545

    update: comments about encapsulation, when its not being used.

--- encapsulation/Main.java
+++ encapsulation/Main.java
@@ -11,6 +11,8 @@ public class Main {
         playerWithoutEncapsulation.loseHealth(damage);
         System.out.println("Remaining health: " + playerWithoutEncapsulation.healthRemaining());
         // this is risky as you can set health to invalid values.
+        // we may have to change the calling code in many places if we change the member variable name
+        // we may have to initialize the player class member variables in many places without a constructor
         playerWithoutEncapsulation.health = 200;
         playerWithoutEncapsulation.loseHealth(11);
         System.out.println("Remaining health: " + playerWithoutEncapsulation.healthRemaining());
